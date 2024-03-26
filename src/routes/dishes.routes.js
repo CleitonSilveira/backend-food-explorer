0163ea1ce466f58d66de8bcfcb671afecafe936a
CleitonSilveira <cleiton.desenvolvimento@plasoft.com.br>
2024-03-26 15:35:38 -0300

    commit

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -9,12 +9,14 @@ const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization"
 const dishesRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
+const dishesController = new DishesController()
+
 dishesRoutes.use(ensureAuthenticated);
 
-// dishesRoutes.get("/:id", DishesController.show);
-// dishesRoutes.get("/", DishesController.index);
-// dishesRoutes.post("/", verifyUserAuthorization("admin"), upload.single("image"), DishesController.create);
-// dishesRoutes.put("/:id", verifyUserAuthorization("admin"), upload.single("image"), DishesController.update);
-// dishesRoutes.delete("/", verifyUserAuthorization("admin"), DishesController.delete);
+dishesRoutes.get("/:id", dishesController.show);
+dishesRoutes.get("/", dishesController.index);
+dishesRoutes.post("/", verifyUserAuthorization("admin"), upload.single("image"), dishesController.create);
+dishesRoutes.put("/:id", verifyUserAuthorization("admin"), upload.single("image"), dishesController.update);
+dishesRoutes.delete("/", verifyUserAuthorization("admin"), dishesController.delete);
 
 module.exports = dishesRoutes;
